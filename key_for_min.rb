1ac522010a9b58540af390a29136870083bcaa62
Joy Evans <joyevans@me.com>
2020-10-15 20:20:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,29 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
+#hash = {:blake => 500, :ashley => 2, :adam => 1}
+
 def key_for_min_value(name_hash)
+  
+  min_value = 500
+  person = ""
+  
+  hash.each do |name, value|
+    if value < min_value
+      person = min_value
+    end
 
 end
+
+person
+
+end
+
+#winner = ""
+#passengers.each do |suite, name|
+ # if suite == :suite_a && name.start_with?("A")
+  #  winner = name
+  #end
+#end
+ 
+#winner
