9a08efa0808e7dcab0026ddf1c6fe4d675ac5666
Joy Evans <joyevans@me.com>
2020-10-15 20:25:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,15 +6,15 @@
 def key_for_min_value(name_hash)
   
   min_value = 0 
-  person = nil
+  empty_hash = nil
   
   name_hash.each do |name, value|
-    if value < min_value
-      person << min_value
-    end
+    if smallest_value == 0 || value < smallest_value
+    smallest_value = value
+    empty_hash = name
+  end
+ end
+ empty_hash
 
 end
-
-person
-
-end
+  
